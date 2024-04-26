7c1f19b08fe53deb1a863564b053a0603cc28428
JoacoLejarraga <162721835+JoacoLejarraga@users.noreply.github.com>
2024-04-26 09:09:27 -0300

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -1,33 +1,68 @@
 def roots(a, b, c):
-	x1 = (- b + ( (b *2) - 4 * a * c)*0.5) / (2 * a)
-	x2 = (- b - ( (b *2) - 4 * a * c)*0.5) / (2 * a)
-	det = (b **2) - 4 * a * c
-	if det > 0:
-		return f"({x1}, {x2})"
-	elif det == 0:
-		return f"({x1})"
-	else:
-		return "( )"
+    
+    radicando = b**2-4*a*c
+    
+    raizP = (-b+((radicando)**(1/2)))/2*a
+    
+    raizN = (-b-((radicando)**(1/2)))/2*a
+
+    if (radicando<0):
+        
+        return "( )"
+        
+    elif (raizP == raizN):
+        
+        return f"({raizP})"
+        
+    else:
+        
+        return f"({raizP}, {raizN})"
+
 def value_y(a, b, c, x):
-	y = (a * (x**2)) + (b * x) + c
-	return y
+    
+    return (a*(x**2) + b*x + c)
+
 def to_string(a, b, c):
-	if a != 0 and b != 0 and c != 0:
-		return f"f(x) = {a} * X^2 + {b} * X + {c}"
-	elif a == 0 and b != 0 and c != 0:
-		return f"f(x) = {b} * X + {c}"
-	elif a == 0 and b == 0 and c != 0: 
-		return f"f(x) = {c}"
-	elif a != 0 and b == 0 and c != 0: 
-		return f"f(x) = {a} * X^2 + {c}"
-	else:
-		return "f(x) = 0"
+
+    if a != 0 and b != 0 and c != 0:
+
+        return f"f(x) = {a} * X^2 + {b} * X + {c}"
+
+    elif a == 0 and b != 0 and c != 0:
+
+        return f"f(x) = {b} * X + {c}"
+
+    elif a == 0 and b == 0 and c != 0: 
+
+        return f"f(x) = {c}"
+
+    elif a != 0 and b == 0 and c != 0:
+
+        return f"f(x) = {a} * X^2 + {c}"
+
+    else:
+
+        return "f(x) = 0"
+    
 def derivation(a, b, c):
-	if a != 0 and b != 0 and c != 0:
-		return f"f'(x) = {2*a} * X + {b}"
-	elif a == 0 and b != 0 and c != 0:
-		return f"f'(x) = {b}"
-	elif a != 0 and b == 0 and c != 0: 
-		return f"f'(x) = {2*a} * X"
-	else:
-		return "f'(x) = 0"
+
+    if a != 0 and b != 0 and c != 0:
+
+        return f"f'(x) = {2*a} * X + {b}"
+
+    elif a == 0 and b != 0 and c != 0:
+
+        return f"f'(x) = {b}"
+
+    elif a != 0 and b == 0 and c != 0: 
+
+        return f"f'(x) = {2*a} * X"
+
+    else:
+
+        return "f'(x) = 0"
+
+print(roots(1,-3,2))
+print(value_y(1,-3,2,-1))
+print(to_string(2,-3,1))
+print(derivation(2,-3,1))
